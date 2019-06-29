d7dfaa957fd954746e6aa61d8674a472239360fe
Arthur Unity <apploidyakutsk@gmail.com>
2019-06-29 09:13:21 +0300

    added user profile dashboard

---
+++ jsx/user-profile/UserDashboard.jsx
@@ -0,0 +1,21 @@
+class UserDashboard extends React.Component {
+    render() {
+        return (
+            <div className="card text-center">
+                <div className="card-header">
+                    <ul className="nav nav-tabs card-header-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="">Последняя активность</a>
+                    </li>
+                    <li class="nav-item">
+                        <a className="nav-link" href="">Диалог</a>
+                    </li>
+                    </ul>
+                </div>
+                <div className="card-body">
+                    <LastActiveTable />
+                </div>
+            </div>
+        )
+    }
+}

9dd138c297cb10399f7fb89e86c2b2d6acf73c28
Arthur Unity <apploidyakutsk@gmail.com>
2019-06-29 09:15:34 +0300

    added react router

--- jsx/script.jsx
+++ jsx/script.jsx
@@ -1,14 +1,39 @@
+
+
 class ContentPage extends React.Component {
+    componentDidMount(){
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+            $(this).toggleClass('active');
+        });
+        });
+    }
+
     render(){
         return (
             <div>
-                <Sidebar/>
+                <Sidebar content={<QueuePage/>}/>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <ContentPage/>,
+    <Sidebar content={
+    <ReactRouterDOM.HashRouter>
+        <div>
+          <div >
+            <ReactRouterDOM.Route exact path="/" component={MainPage}/>
+            <ReactRouterDOM.Route exact path="/queue/:id" component={QueuePage}/>
+            <ReactRouterDOM.Route exact path="/signin" component={SignInPage} />
+            <ReactRouterDOM.Route exact path="/register" component={RegisterPage} />
+            <ReactRouterDOM.Route exact path="/create" component={CreateQueuePage} />
+            <ReactRouterDOM.Route exact path="/myprofile" component={UserProfile} />
+            <ReactRouterDOM.Route exact path="/chat/:id" component={Chat} />
+          </div>
+        </div>
+      </ReactRouterDOM.HashRouter>} />
+    ,
     document.getElementById('contentReact')
 )

1df3dbace8244f7781ce9b37cc036ba67393541b
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-09 11:51:12 +0900

    small changes

--- compiled/script.js
+++ compiled/script.js
@@ -1,18 +1,10 @@
 
 
 class ContentPage extends React.Component {
-    componentDidMount() {
-        $(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-                $(this).toggleClass('active');
-            });
-        });
-    }
 
     render() {
         return React.createElement(
-            'div',
+            "div",
             null,
             React.createElement(Sidebar, { content: React.createElement(QueuePage, null) })
         );
@@ -23,18 +15,18 @@ ReactDOM.render(React.createElement(Sidebar, { content: React.createElement(
         ReactRouterDOM.HashRouter,
         null,
         React.createElement(
-            'div',
+            "div",
             null,
             React.createElement(
-                'div',
+                "div",
                 null,
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/', component: MainPage }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/queue/:id', component: QueuePage }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/signin', component: SignInPage }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/register', component: RegisterPage }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/create', component: CreateQueuePage }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/myprofile', component: UserProfile }),
-                React.createElement(ReactRouterDOM.Route, { exact: true, path: '/chat/:id', component: Chat })
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/", component: MainPage }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/queue/:id", component: QueuePage }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/signin", component: SignInPage }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/register", component: RegisterPage }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/create", component: CreateQueuePage }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/myprofile", component: UserProfile }),
+                React.createElement(ReactRouterDOM.Route, { exact: true, path: "/chat/:id", component: Chat })
             )
         )
     ) }), document.getElementById('contentReact'));

0df3850cb91263339d526a6995ccb6ccd1ab89c9
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-10 22:29:59 +0900

    added passwords, email (sync) validation with css + states

--- jsx/RegisterPage.jsx
+++ jsx/RegisterPage.jsx
@@ -1,29 +1,56 @@
+// TODO: add async input validation to email, name, lastname
+
 const React = require('react')
 
 class RegisterPage extends React.Component {
     constructor(props){
         super(props);
-        this.state = {"name" : "", "email" : "", "password" : "", "passConfirm" : ""}
+        this.state = {"name" : "","lastname": "", "email" : "", "password" : "", "passConfirm" : "", "passwordClass": "", "passConfirmClass": ""}
         this.nameInpChange = this.nameInpChange.bind(this);
+        this.lastnameInpChange = this.lastnameInpChange.bind(this);
         this.emailInpChange = this.emailInpChange.bind(this);
         this.passInpChange = this.passInpChange.bind(this);
         this.confPassInpChange = this.confPassInpChange.bind(this);
+        this.register = this.register = this.register.bind(this);
     }
 
     nameInpChange(event){
-        this.setState({"name" : event.value});
+        this.setState({"name" : event.target.value});
+    }
+    lastnameInpChange(event){
+        this.setState({"lastname" : event.target.value});
     }
 
     emailInpChange(event){
-        this.setState({"email" : event.value});
+        this.setState({"email" : event.target.value});
     }
 
     passInpChange(event){
-        this.setState({"password" : event.value});
+        if (/[^\s]+/.test(event.target.value) && event.target.value.length > 8){
+            this.setState({"passwordClass" : "is-valid", "password" : event.target.value})
+        } else {
+            this.setState({"passwordClass" : "", "password" : event.target.value})
+        }
     }
 
     confPassInpChange(event){
-        this.setState({"passConfirm" : event.value});
+        if (this.state.password === event.target.value){
+            this.setState({"passConfirmClass" : "is-valid","passConfirm" : event.target.value})
+        } else {
+            this.setState({"passConfirmClass" : "is-invalid" , "passConfirm" : event.target.value});
+        }
+    }
+
+    register(event){
+        if (this.state.passwordClass === "is-valid" && this.state.passConfirmClass === "is-valid"){
+            if (/[^\s]+/.test(this.state.name) && /[^s]+/.test(this.state.lastname) && this.state.name.length < 15 && this.state.lastname < 20){
+                if (/[^\s]+@[^\s]+/.test(this.state.email)){
+                    console.log("sending request")
+                    return;
+                }
+            }
+        }
+        console.log("failed")
     }
 
     render(){
@@ -33,25 +60,25 @@ class RegisterPage extends React.Component {
                         <div className="col-6 justify-content-center">
                             <div className="form-group row">
                                 <div className="col-md-6">
-                                    <input type="text" className="form-control" placeholder="Имя" value={this.state.name} onChange={this.nameInpChange}/>
+                                    <input type="text" className="form-control was-validated" placeholder="Имя" value={this.state.name} onChange={this.nameInpChange}/>
                                 </div>
                                 <div className="col-md-6">
-                                    <input type="text" className="form-control" placeholder="Фамилия" value={this.state.name} onChange={this.nameInpChange}/>
+                                    <input type="text" className="form-control was-validated" placeholder="Фамилия" value={this.state.lastname} onChange={this.lastnameInpChange}/>
                                 </div>
                                 
                                 
                             </div>
                             <div className="form-group">
-                                <input type="email" className="form-control" placeholder="Электронная почта" value={this.state.email} onChange={this.emailInpChange}/>
+                                <input type="email" className="form-control was-validated" placeholder="Электронная почта" value={this.state.email} onChange={this.emailInpChange}/>
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Your Password" value={this.state.password} onChange={this.passInpChange}/>
+                                <input type="password" className={"form-control " + this.state.passwordClass} placeholder="Your Password" value={this.state.password} onChange={this.passInpChange}/>
                             </div>
                             <div className="form-group">
-                                <input type="password" className="form-control" placeholder="Confirm Password" value={this.state.passConfirm} onChange={this.confPassInpChange}/>
+                                <input type="password" className={"form-control " + this.state.passConfirmClass} placeholder="Confirm Password" value={this.state.passConfirm} onChange={this.confPassInpChange}/>
                             </div>
                             <div className="form-group mx-auto">
-                            <button type="button" className="btn-primary btn justify-content-center mx-auto">Регистрация</button>
+                            <button type="button" onClick={this.register} className="btn-primary btn justify-content-center mx-auto">Регистрация</button>
                             </div>
                             
                         </div>

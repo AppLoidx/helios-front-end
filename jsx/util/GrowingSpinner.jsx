f115f5e2bdf7f88a1e2f7c840ac4b884a3a4a92a
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-17 15:49:52 +0900

    added growing spinner jsx-component

---
+++ jsx/util/GrowingSpinner.jsx
@@ -0,0 +1,16 @@
+const React = require("react");
+
+class GrowingSpinner extends React.Component{
+    render(){
+        return(
+            <div className="d-flex justify-content-center">
+                <div className="spinner-grow spinner-grow-lg text-success" style={{"width": "3rem", "height": "3rem"}} role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+
+            </div>)
+
+    }
+}
+
+module.exports = GrowingSpinner;

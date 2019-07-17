b5f52b2302c0277a2b4b1ffdf3d7c826b3f7c4d6
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-17 22:07:56 +0900

    new compiled version

--- js/bundle.js
+++ js/bundle.js
@@ -491,6 +491,7 @@ module.exports = Sidebar;
 /***/ (function(module, exports, __webpack_require__) {
 
 const React = __webpack_require__(/*! react */ "./node_modules/react/react.js");
+const RoundedSpinner = __webpack_require__(/*! ./../util/RoundedSpinner.jsx */ "./jsx/util/RoundedSpinner.jsx");
 
 class CreateQueuePage extends React.Component {
     // componentDidMount(){
@@ -514,16 +515,22 @@ class CreateQueuePageContent extends React.Component {
         this.handleNameInput = this.handleNameInput.bind(this);
         this.handleFullnameInput = this.handleFullnameInput.bind(this);
         this.sendRequest = this.sendRequest.bind(this);
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+        this.privateCheckClick = this.privateCheckClick.bind(this);
+        this.autoCheckClick = this.autoCheckClick.bind(this);
         this.state = { "sending": false, "successful": false, "fullname": "", "queueName": "",
-            "inputNameClass": "", "submitButtonClass": "disabled", "collapseTarget": "" };
+            "inputNameClass": "", "submitButtonClass": "disabled", "collapseTarget": "",
+            "collapseComponentId": "collapseSend", "queueNameForCollapse": "",
+            "auto": false, "private": false, "password": "", "passwordClass": "" };
     }
 
     sendRequest() {
-        if (this.state.submitButtonClass === "disabled") {
-            return 2;
+        if (this.state.submitButtonClass === "disabled" || this.state.private && this.state.password === "") {
+            return;
         }
         console.log("sending");
-        this.setState({ "sending": true, "collapseTarget": "" });
+        this.setState({ "sending": true, "collapseTarget": "", "collapseComponentId": "collapseSendOpened",
+            "queueNameForCollapse": this.state.fullname });
         fetch("http://localhost:8080/mavenserver_war/api/queue?" + "queueName=" + this.state.queueName + "&" + "fullname=" + this.state.fullname + "&" + "session=MTIzS3Vwcml5YW5vdi0xNDQ1NTQwMzc1c2FsdDQw", { "method": "POST" }).then(response => {
 
             if (response.ok) {
@@ -541,7 +548,7 @@ class CreateQueuePageContent extends React.Component {
 
     handleNameInput(event) {
         if (/^[a-zA-Z0-9-_]+$/.test(event.target.value)) {
-            if (/[^\s]+/.test(document.getElementById("inputFullname").value)) {
+            if (/[^\s]+/.test(document.getElementById("inputFullname").value) && (!this.state.private || /[^\s]+/.test(this.state.password))) {
                 this.setState({ "inputNameClass": "is-valid", "submitButtonClass": "", "collapseTarget": "#collapseSend" });
             } else {
                 this.setState({ "inputNameClass": "is-valid", "submitButtonClass": "disabled", "collapseTarget": "" });
@@ -553,89 +560,160 @@ class CreateQueuePageContent extends React.Component {
     }
 
     handleFullnameInput(event) {
-        if (/[^\s]+/.test(event.target.value) && this.state.inputNameClass === "is-valid") {
+        if (/[^\s]+/.test(event.target.value) && this.state.inputNameClass === "is-valid" && (!this.state.private || /[^\s]+/.test(this.state.password))) {
             this.setState({ "submitButtonClass": "", "collapseTarget": "#collapseSend" });
         } else {
             this.setState({ "submitButtonClass": "disabled", "collapseTarget": "" });
         }
-        console.log(this.state.allValid);
         this.setState({ "fullname": event.target.value });
+    }
+
+    handlePasswordInput(event) {
+
+        if (/[^\s]+/.test(event.target.value)) {
+            this.setState({ "passwordClass": "is-valid" });
+        } else {
+            this.setState({ "passwordClass": "is-invalid" });
+        }
+
+        if (/[^\s]+/.test(document.getElementById("inputFullname").value) && /[^\s]+/.test(document.getElementById("inputName").value) && /[^\s]+/.test(event.target.value)) {
+            this.setState({ "submitButtonClass": "", "collapseTarget": "#collapseSend" });
+        } else {
+            this.setState({ "submitButtonClass": "disabled", "collapseTarget": "" });
+        }
+
+        this.setState({ "password": event.target.value });
+    }
+
+    privateCheckClick(event) {
+        this.setState({ "private": event.target.checked });
+        if (/[^\s]+/.test(document.getElementById("inputFullname").value) && /[^\s]+/.test(document.getElementById("inputName").value) && (!event.target.checked || /[^\s]+/.test(this.state.password))) {
+            this.setState({ "submitButtonClass": "", "collapseTarget": "#collapseSend" });
+        } else {
+            this.setState({ "submitButtonClass": "disabled", "collapseTarget": "" });
+        }
+    }
+
+    autoCheckClick(event) {
+        this.setState({ "auto": event.target.checked });
     }
 
     render() {
         return React.createElement(
-            "div",
+            'div',
             null,
             React.createElement(
-                "form",
-                { className: "form-signin" },
+                'form',
+                { className: 'form-signin' },
                 React.createElement(
-                    "div",
-                    { className: "text-center mb-4" },
+                    'div',
+                    { className: 'text-center mb-4' },
                     React.createElement(
-                        "h1",
-                        { className: "display mb-3 font-weight-normal" },
-                        "HELIOS"
+                        'h1',
+                        { className: 'display mb-3 font-weight-normal' },
+                        'HELIOS'
                     ),
                     React.createElement(
-                        "p",
+                        'p',
                         null,
-                        "\u0415\u0441\u043B\u0438 \u0432\u044B \u043D\u0435 \u0437\u043D\u0430\u0435\u0442\u0435 \u0447\u0442\u043E \u0434\u0435\u043B\u0430\u0442\u044C, \u0441\u043C\u043E\u0442\u0440\u0438\u0442\u0435 ",
+                        '\u0415\u0441\u043B\u0438 \u0432\u044B \u043D\u0435 \u0437\u043D\u0430\u0435\u0442\u0435 \u0447\u0442\u043E \u0434\u0435\u043B\u0430\u0442\u044C, \u0441\u043C\u043E\u0442\u0440\u0438\u0442\u0435 ',
                         React.createElement(
-                            "a",
-                            { href: "#", className: "text-primary" },
-                            "\u0414\u043E\u043A\u0443\u043C\u0435\u043D\u0442\u0430\u0446\u0438\u044E"
+                            'a',
+                            { href: '#', className: 'text-primary' },
+                            '\u0414\u043E\u043A\u0443\u043C\u0435\u043D\u0442\u0430\u0446\u0438\u044E'
                         )
                     )
                 ),
                 React.createElement(
-                    "div",
-                    { className: "form-group" },
-                    React.createElement("input", { type: "text", id: "inputName", className: "form-control " + this.state.inputNameClass, placeholder: "\u041A\u043E\u0440\u043E\u0442\u043A\u043E\u0435 \u0438\u043C\u044F \u0434\u043B\u044F \u0441\u0441\u044B\u043B\u043A\u0438", name: "queueName", value: this.state.queueName, onChange: this.handleNameInput, required: true, autoFocus: true }),
-                    React.createElement("label", { htmlFor: "inputName" })
+                    'div',
+                    { className: 'form-group' },
+                    React.createElement('input', { type: 'text', id: 'inputName', className: "form-control " + this.state.inputNameClass, placeholder: '\u041A\u043E\u0440\u043E\u0442\u043A\u043E\u0435 \u0438\u043C\u044F \u0434\u043B\u044F \u0441\u0441\u044B\u043B\u043A\u0438', name: 'queueName', value: this.state.queueName, onChange: this.handleNameInput, required: true, autoFocus: true })
                 ),
                 React.createElement(
-                    "div",
-                    { className: "form-group" },
-                    React.createElement("input", { type: "text", id: "inputFullname", className: "form-control", name: "fullname", value: this.state.fullname, placeholder: "\u041F\u043E\u043B\u043D\u043E\u0435 \u0438\u043C\u044F \u043E\u0447\u0435\u0440\u0435\u0434\u0438", onChange: this.handleFullnameInput, required: true }),
-                    React.createElement("label", { htmlFor: "inputFullname" })
+                    'div',
+                    { className: 'form-group' },
+                    React.createElement('input', { type: 'text', id: 'inputFullname', className: 'form-control', name: 'fullname', value: this.state.fullname, placeholder: '\u041F\u043E\u043B\u043D\u043E\u0435 \u0438\u043C\u044F \u043E\u0447\u0435\u0440\u0435\u0434\u0438', onChange: this.handleFullnameInput, required: true })
                 ),
                 React.createElement(
-                    "p",
+                    'div',
+                    { className: 'row justify-content-center justify-content-between ml-2' },
+                    React.createElement(
+                        'div',
+                        { className: 'form-check form-group col-auto' },
+                        React.createElement('input', { className: 'form-check-input', type: 'checkbox', value: '', id: 'privateCheck', onClick: this.privateCheckClick }),
+                        React.createElement(
+                            'label',
+                            { className: 'form-check-label', htmlFor: 'privateCheck' },
+                            '\u0417\u0430\u043A\u0440\u044B\u0442\u0430\u044F'
+                        ),
+                        React.createElement(
+                            'span',
+                            { className: "ml-1" },
+                            React.createElement('i', { className: "fa fa-question-circle", 'data-toggle': "tooltip", title: "Очередь с паролью" })
+                        )
+                    ),
+                    React.createElement(
+                        'div',
+                        { className: 'form-check form-group col-auto' },
+                        React.createElement('input', { className: 'form-check-input', type: 'checkbox', value: '', id: 'autoCheck', onClick: this.autoCheckClick }),
+                        React.createElement(
+                            'label',
+                            { className: 'form-check-label', htmlFor: 'autoCheck' },
+                            '\u0410\u0432\u0442\u043E\u0433\u0435\u043D\u0435\u0440\u0438\u0440\u0443\u0435\u043C\u0430\u044F'
+                        ),
+                        React.createElement(
+                            'span',
+                            { className: "ml-1" },
+                            React.createElement('i', { className: "fa fa-question-circle", 'data-toggle': 'tooltip', title: "Очередь которая будет автоматически генерироваться" })
+                        )
+                    )
+                ),
+                this.state.private ? React.createElement(
+                    'div',
+                    { className: 'form-group' },
+                    React.createElement('input', { type: 'text', id: 'queuePassword', className: "form-control " + this.state.passwordClass, name: 'Password', value: this.state.password, placeholder: '\u041F\u0430\u0440\u043E\u043B\u044C \u043E\u0447\u0435\u0440\u0435\u0434\u0438', onChange: this.handlePasswordInput, required: true })
+                ) : React.createElement('span', null),
+                React.createElement(
+                    'p',
                     null,
                     React.createElement(
-                        "button",
-                        { className: "btn btn-primary " + this.state.submitButtonClass, type: "button", "data-toggle": "collapse", "data-target": this.state.collapseTarget, "aria-expanded": "false", "aria-controls": "collapseSend", onClick: this.sendRequest },
-                        "\u0421\u043E\u0437\u0434\u0430\u0442\u044C"
+                        'button',
+                        { className: "btn btn-primary " + this.state.submitButtonClass, type: 'button', 'data-toggle': 'collapse', 'data-target': this.state.collapseTarget, 'aria-expanded': 'false', 'aria-controls': 'collapseSend', onClick: this.sendRequest },
+                        '\u0421\u043E\u0437\u0434\u0430\u0442\u044C'
                     )
                 ),
                 React.createElement(
-                    "div",
-                    { className: "collapse", id: "collapseSend" },
+                    'div',
+                    { className: 'collapse', id: this.state.collapseComponentId },
                     React.createElement(
-                        "div",
-                        { className: "card card-body text-center" },
+                        'div',
+                        { className: 'card card-body text-center' },
                         this.state.sending ? React.createElement(
-                            "p",
+                            'div',
                             null,
-                            "\u041E\u0442\u043F\u0440\u0430\u0432\u043A\u0430 \u0434\u0430\u043D\u043D\u044B\u0445"
+                            React.createElement(
+                                'p',
+                                null,
+                                '\u0421\u043E\u0437\u0434\u0430\u043D\u0438\u0435 \u043E\u0447\u0435\u0440\u0435\u0434\u0438'
+                            ),
+                            React.createElement(RoundedSpinner, null)
                         ) : this.state.successful ? React.createElement(
-                            "p",
-                            { className: "text-success" },
-                            "\u041E\u0447\u0435\u0440\u0435\u0434\u044C \u0443\u0441\u043F\u0435\u0448\u043D\u043E \u0441\u043E\u0437\u0434\u0430\u043D\u0430"
+                            'p',
+                            { className: 'text-success' },
+                            '\u041E\u0447\u0435\u0440\u0435\u0434\u044C \u0443\u0441\u043F\u0435\u0448\u043D\u043E \u0441\u043E\u0437\u0434\u0430\u043D\u0430'
                         ) : React.createElement(
-                            "p",
-                            { className: "text-danger" },
-                            "\u041D\u0435 \u0443\u0434\u0430\u043B\u043E\u0441\u044C \u0441\u043E\u0437\u0434\u0430\u0442\u044C \u043E\u0447\u0435\u0440\u0435\u0434\u044C. ",
-                            React.createElement("br", null),
-                            this.state.fullname
+                            'p',
+                            { className: 'text-danger' },
+                            '\u041D\u0435 \u0443\u0434\u0430\u043B\u043E\u0441\u044C \u0441\u043E\u0437\u0434\u0430\u0442\u044C \u043E\u0447\u0435\u0440\u0435\u0434\u044C. ',
+                            React.createElement('br', null),
+                            this.state.queueNameForCollapse
                         )
                     )
                 ),
                 React.createElement(
-                    "p",
-                    { className: "mt-5 mb-3 text-muted text-center" },
-                    "\xA9 ITMO 2019"
+                    'p',
+                    { className: 'mt-5 mb-3 text-muted text-center' },
+                    '\xA9 ITMO 2019'
                 )
             )
         );
@@ -1459,6 +1537,37 @@ class GrowingSpinner extends React.Component {
 }
 
 module.exports = GrowingSpinner;
+
+/***/ }),
+
+/***/ "./jsx/util/RoundedSpinner.jsx":
+/*!*************************************!*\
+  !*** ./jsx/util/RoundedSpinner.jsx ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+const React = __webpack_require__(/*! react */ "./node_modules/react/react.js");
+
+class RoundedSpinner extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return React.createElement(
+            "div",
+            { className: "spinner-border", role: "status" },
+            React.createElement(
+                "span",
+                { className: "sr-only" },
+                "Loading..."
+            )
+        );
+    }
+}
+
+module.exports = RoundedSpinner;
 
 /***/ }),
 
@@ -23650,7 +23759,7 @@ if (false) {} else {
 /*!***************************************************************!*\
   !*** ./node_modules/react-router-dom/esm/react-router-dom.js ***!
   \***************************************************************/
-/*! exports provided: BrowserRouter, HashRouter, Link, NavLink, MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext */
+/*! exports provided: MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext, BrowserRouter, HashRouter, Link, NavLink */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";

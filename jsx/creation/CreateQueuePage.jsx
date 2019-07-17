d328f19e3956a84697c7dcbc0d42c022f267ff4f
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-17 22:08:38 +0900

    added checkboxes: private x auto

--- jsx/creation/CreateQueuePage.jsx
+++ jsx/creation/CreateQueuePage.jsx
@@ -1,4 +1,5 @@
-const React = require('react')
+const React = require('react');
+const RoundedSpinner = require('./../util/RoundedSpinner.jsx');
 
 class CreateQueuePage extends React.Component {
     // componentDidMount(){
@@ -22,16 +23,23 @@ class CreateQueuePageContent extends React.Component {
         this.handleNameInput = this.handleNameInput.bind(this);
         this.handleFullnameInput = this.handleFullnameInput.bind(this);
         this.sendRequest = this.sendRequest.bind(this);
+        this.handlePasswordInput = this.handlePasswordInput.bind(this);
+        this.privateCheckClick = this.privateCheckClick.bind(this);
+        this.autoCheckClick = this.autoCheckClick.bind(this);
         this.state = {"sending" : false, "successful" : false, "fullname" : "", "queueName" : "",
-                            "inputNameClass" : "", "submitButtonClass" : "disabled", "collapseTarget" : ""}
+                            "inputNameClass" : "", "submitButtonClass" : "disabled", "collapseTarget" : "",
+                        "collapseComponentId" : "collapseSend", "queueNameForCollapse": "",
+                        "auto" : false, "private" : false, "password" : "", "passwordClass" : ""}
+
     }
 
     sendRequest(){
-        if (this.state.submitButtonClass === "disabled"){
-            return 2;
+        if (this.state.submitButtonClass === "disabled" || (this.state.private && this.state.password === "")){
+            return;
         }
-        console.log("sending")
-        this.setState({"sending" : true, "collapseTarget" : ""})
+        console.log("sending");
+        this.setState({"sending" : true, "collapseTarget" : "", "collapseComponentId" : "collapseSendOpened"
+                            ,"queueNameForCollapse" : this.state.fullname});
         fetch("http://localhost:8080/mavenserver_war/api/queue?"
         + "queueName=" + this.state.queueName + "&"
         + "fullname=" + this.state.fullname + "&"
@@ -55,7 +63,7 @@ class CreateQueuePageContent extends React.Component {
 
     handleNameInput(event){
         if (/^[a-zA-Z0-9-_]+$/.test(event.target.value)){
-            if (/[^\s]+/.test(document.getElementById("inputFullname").value)){
+            if (/[^\s]+/.test(document.getElementById("inputFullname").value) && (!this.state.private || /[^\s]+/.test(this.state.password))){
                 this.setState({"inputNameClass" : "is-valid", "submitButtonClass" : "", "collapseTarget" : "#collapseSend"})
             } else {
                 this.setState({"inputNameClass" : "is-valid", "submitButtonClass" : "disabled", "collapseTarget" : ""})
@@ -68,13 +76,45 @@ class CreateQueuePageContent extends React.Component {
     }
 
     handleFullnameInput(event){
-        if (/[^\s]+/.test(event.target.value) && this.state.inputNameClass === "is-valid"){
+        if (/[^\s]+/.test(event.target.value) && this.state.inputNameClass === "is-valid" && (!this.state.private || /[^\s]+/.test(this.state.password))){
                 this.setState({"submitButtonClass" : "", "collapseTarget" : "#collapseSend"})
         } else {
                 this.setState({"submitButtonClass" : "disabled", "collapseTarget" : ""})
         }
-        console.log(this.state.allValid);
         this.setState({"fullname" : event.target.value});
+    }
+
+    handlePasswordInput(event){
+
+        if (/[^\s]+/.test(event.target.value)){
+            this.setState({"passwordClass" : "is-valid"})
+        } else {
+            this.setState({"passwordClass" : "is-invalid"})
+        }
+
+        if (/[^\s]+/.test(document.getElementById("inputFullname").value) && /[^\s]+/.test(document.getElementById("inputName").value) && /[^\s]+/.test(event.target.value) ){
+            this.setState({"submitButtonClass" : "", "collapseTarget" : "#collapseSend"})
+        } else {
+            this.setState({"submitButtonClass" : "disabled", "collapseTarget" : ""})
+        }
+
+        this.setState({"password" : event.target.value});
+
+
+    }
+
+    privateCheckClick(event){
+        this.setState({"private" : event.target.checked});
+        if (/[^\s]+/.test(document.getElementById("inputFullname").value) && /[^\s]+/.test(document.getElementById("inputName").value) && (!event.target.checked || /[^\s]+/.test(this.state.password)) ){
+            this.setState({"submitButtonClass" : "", "collapseTarget" : "#collapseSend"})
+        } else {
+            this.setState({"submitButtonClass" : "disabled", "collapseTarget" : ""})
+        }
+    }
+
+    autoCheckClick(event){
+        this.setState({"auto" : event.target.checked});
+
     }
 
     render(){
@@ -88,25 +128,47 @@ class CreateQueuePageContent extends React.Component {
 
             <div className="form-group">
                 <input type="text" id="inputName" className={"form-control " + this.state.inputNameClass} placeholder="Короткое имя для ссылки" name="queueName" value={this.state.queueName} onChange={this.handleNameInput} required autoFocus/>
-                <label htmlFor="inputName"></label>
             </div>
 
             <div className="form-group">
                 <input type="text" id="inputFullname" className="form-control" name="fullname" value={this.state.fullname} placeholder="Полное имя очереди" onChange={this.handleFullnameInput} required/>
-                <label htmlFor="inputFullname"></label>
             </div>
+
+            <div className = "row justify-content-center justify-content-between ml-2">
+                <div className="form-check form-group col-auto">
+                    <input className="form-check-input" type="checkbox" value="" id="privateCheck" onClick={this.privateCheckClick}/>
+                    <label className="form-check-label" htmlFor="privateCheck">Закрытая</label>
+                    <span className={"ml-1"}>
+                        <i className={"fa fa-question-circle"} data-toggle={"tooltip"} title={"Очередь с паролью"} />
+                    </span>
+                </div>
+                <div className="form-check form-group col-auto">
+                    <input className="form-check-input" type="checkbox" value="" id="autoCheck" onClick={this.autoCheckClick}/>
+                    <label className="form-check-label" htmlFor="autoCheck">Автогенерируемая</label>
+                    <span className={"ml-1"}>
+                        <i className={"fa fa-question-circle"} data-toggle="tooltip" title={"Очередь которая будет автоматически генерироваться"} />
+                    </span>
+                </div>
+            </div>
+
+            {this.state.private?
+                <div className="form-group">
+                    <input type="text" id="queuePassword" className={"form-control "+this.state.passwordClass} name="Password" value={this.state.password} placeholder="Пароль очереди" onChange={this.handlePasswordInput} required/>
+                </div>
+            :
+            <span></span>}
 
             <p>
             <button className={"btn btn-primary " + this.state.submitButtonClass} type="button" data-toggle="collapse" data-target={this.state.collapseTarget} aria-expanded="false" aria-controls="collapseSend" onClick={this.sendRequest}>
                 Создать
             </button>
             </p>
-            <div className="collapse" id="collapseSend">
+            <div className="collapse" id={this.state.collapseComponentId}>
                 <div className="card card-body text-center">
-                    {this.state.sending?<p>Отправка данных</p>
+                    {this.state.sending?<div><p>Создание очереди</p><RoundedSpinner/></div>
                     :
                     this.state.successful?<p className="text-success">Очередь успешно создана</p>:
-                    <p className="text-danger">Не удалось создать очередь. <br/>{this.state.fullname}</p>}
+                    <p className="text-danger">Не удалось создать очередь. <br/>{this.state.queueNameForCollapse}</p>}
                 </div>
             </div>
             
@@ -118,4 +180,4 @@ class CreateQueuePageContent extends React.Component {
     }
 }
 
-module.exports = CreateQueuePage
+module.exports = CreateQueuePage;

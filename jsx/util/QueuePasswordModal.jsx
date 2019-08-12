481a6e94a45b5deab2a45459fccf916ac66b82cd
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-12 15:58:57 +0900

    added spinner while send request for password modal

--- jsx/util/QueuePasswordModal.jsx
+++ jsx/util/QueuePasswordModal.jsx
@@ -1,28 +1,28 @@
 const React = require('react');
 const Modal = require('react-bootstrap/Modal.js');
 const Button = require('react-bootstrap/Button');
+const RoundedSpinner = require('./../util/RoundedSpinner.jsx');
 
 class QueuePasswordModal extends React.Component {
     constructor(props){
         super(props);
-        this.state = {password : ""};
+        this.state = {password : "", successful : true, sendingReq: false};
         this.onPasswordChange = this.onPasswordChange.bind(this);
         this.onJoinButtonClick = this.onJoinButtonClick.bind(this);
     }
     onJoinButtonClick(){
-        if (this.state.joinButtonAdditionalClass === "btn-secondary") return;
         this.setState({sendingReq: true});
         fetch("api/queue?queue_name=" + this.props.shortName + "&password=" + this.state.password, {method: "put"})
             .then(resp => {
                 if (resp.status === 200){
+                    this.setState({successful: true});
                     document.location.href = "#/queue/" + this.props.shortName;
                 } else if (resp.status === 403){
-                    this.setState({showModal: true})
+                    this.setState({successful: false})
                 }
 
                 this.setState({sendingReq : false});
             })
-
             .catch(this.setState({sendingReq : false}))
     }
 
@@ -39,11 +39,16 @@ class QueuePasswordModal extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h4>Очередь {this.props.fullName} имеет пароль. <br/>Введите его, чтобы войти в нее</h4>
-                    <form action="" className={"mx-auto"}>
-                        <label htmlFor="#password-modal-input">Пароль: </label>
-                        <input className={"mx-auto"} type="password" id={"password-modal-input"} onChange={this.onPasswordChange}/>
-                    </form>
+                    {this.state.sendingReq?<RoundedSpinner className={"mx-auto"}/>:
+                    <div>
+                        Очередь {this.props.fullName} имеет пароль. <br/>Введите его, чтобы войти в нее<br/>
+                        {this.state.successful?"":<p className={"text-danger"}>Неверный пароль</p>}
+                        <form action="" className={"mx-auto justify-content-center"}>
+                            <label htmlFor="#password-modal-input">Пароль: </label>
+                            <input className={"mx-auto"} type="password" id={"password-modal-input"} onChange={this.onPasswordChange}/>
+                        </form>
+                    </div>}
+
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.onJoinButtonClick}>Подтвердить</Button>

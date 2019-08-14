f8cafde12776109aade9996cd8a4a5676329ffee
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-14 14:08:53 +0900

    added image to main page

--- jsx/MainPage.jsx
+++ jsx/MainPage.jsx
@@ -12,7 +12,10 @@ class MainPage extends React.Component {
 class MainPageContent extends React.Component {
     render(){
         return <div className="mx-auto">
-            <h3 className='display-4 mx-auto text-center mt-5'>Welcome to Helios queue service!</h3>
+            <h3 className='display-4 mx-auto text-center mt-5'>Здесь что-то должно было быть...</h3>
+            <div className={"mx-auto text-center"}>
+                <img className="img-fluid" src="assets/img/menhera-witch.jpg" alt=""/>
+            </div>
         </div>
     }
 }

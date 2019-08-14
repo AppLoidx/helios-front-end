488eed9d3f08f062404faf12d0855922518ef26c
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-14 14:52:59 +0900

    deleted group name

--- jsx/user-profile/UserCard.jsx
+++ jsx/user-profile/UserCard.jsx
@@ -6,9 +6,8 @@ class UserCard extends React.Component {
             <div className="card" style={{width: "18rem"}}>
                 <img src="https://cdn3.iconfinder.com/data/icons/avatars-flat/33/man_4-512.png" className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h3 className="card-title">{this.props.username}</h3>
-                    <h5 className="text-muted">{this.props.fullname}</h5>
-                    <h6>P3112, 1 курс</h6>
+                    <h3 className="card-title">{this.props.fullname}</h3>
+                    <h5 className="text-muted">{this.props.username}</h5>
                 </div>
             </div>
         )

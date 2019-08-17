e686e0a1fc5c67dec6be02c15288a20be50e2a50
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-17 11:26:47 +0900

    added empty state added

--- jsx/Sidebar.jsx
+++ jsx/Sidebar.jsx
@@ -5,7 +5,7 @@ const Spinner = require('./util/RoundedSpinner.jsx');
 class Sidebar extends React.Component {
     constructor(props){
         super(props);
-        this.state = {"queues" : [], "loading" : true, "username" : "", "logged" : false};
+        this.state = {"queues" : [],"queuesEmpty" : false, "loading" : true, "username" : "", "logged" : false};
         this.fetchQueues = this.fetchQueues.bind(this);
     }
 
@@ -22,9 +22,14 @@ class Sidebar extends React.Component {
         }).then(
             resp =>
             {
+                let queues = resp['queues'];
+                queues.sort(function(a, b) {
+                    return a[0] === b[0] ? a > b : a[0] > b[0]
+                });
                 let fullname = resp['user']['first_name'] + " " + resp['user']['last_name'];
                 this.setState({
-                    "queues" : resp['queues'],
+                    "queues" : queues,
+                    queuesEmpty : queues.length === 0,
                     "loading" : false,
                     "username": fullname,
                     "logged" : true})
@@ -36,8 +41,13 @@ class Sidebar extends React.Component {
 
     fetchQueues(){
         fetch('api/user')
-        .then( response => response.json()).then(resp =>
-            this.setState({"queues" : resp['queues'], "loading" : false}));
+        .then( response => response.json()).then(resp =>{
+            let queues = resp['queues'];
+            queues.sort(function(a, b) {
+                return a[0] === b[0] ? a > b : a[0] > b[0]
+            });
+            this.setState({"queues" : queues, "loading" : false, queuesEmpty : queues.length === 0,});
+    })
     }
 
     render(){
@@ -60,11 +70,11 @@ class Sidebar extends React.Component {
                     <ul className="collapse list-unstyled sidebar-link" id="homeSubmenu">
                         {this.state.loading?(<li className="justify-content-center mx-auto text-center"><Spinner /></li>)
                                             :
-                        this.state.queues.map((i, k) => {return <li key={i[0]}><QueueLink link={"#/queue/" + i[0]} name={i[1]}/></li>})
+                        this.state.queuesEmpty?<li className="justify-content-center mx-auto text-center">Пусто</li>:this.state.queues.map((i, k) => {return <li key={k}><QueueLink link={"#/queue/" + i[0]} name={i[1]}/></li>})
                         }
                     </ul>
                 </li>
-                <li><a href={"#/search"} className={"sidebar-link"}>Присоедениться</a></li>
+                <li><a href={"#/search"} className={"sidebar-link"}>Присоединиться</a></li>
                 <li><a href={"#/create"} className={"sidebar-link"}>Создать</a></li>
                 <li>{this.state.logged?
                     <div className={"d-flex justify-content-between py-1"}>

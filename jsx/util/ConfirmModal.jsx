48d9fed515d88d8fc10e55f16e497e7cc14a9602
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-17 14:50:55 +0900

    added spinner for confirm modal

--- jsx/util/ConfirmModal.jsx
+++ jsx/util/ConfirmModal.jsx
@@ -1,6 +1,6 @@
 const React = require('react');
 const Modal = require('react-bootstrap/Modal.js');
-const Button = require('react-bootstrap/Button.js');
+const RoundedSpinner = require('./../util/RoundedSpinner.jsx');
 
 class ConfirmModal extends React.Component {
     render(){
@@ -12,10 +12,11 @@ class ConfirmModal extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
+                    {this.props.sendingrequest?<div className={"mx-auto text-center text-primary"}><RoundedSpinner /></div>:
                     <ul className="col justify-content-center">
                         <li className={"text-center my-4"} style={{listStyle : 'none'}}   ><p className={""}>{this.props.message}</p></li>
-                        <li className={"text-center my-4"} style={{listStyle : 'none'}}   ><button className="btn btn-primary col-4 mx-1" onClick={this.props.yes}>Да</button><button className="btn btn-secondary col-4 mx-1" onClick={this.props.no}>Отмена</button></li>
-                    </ul>
+                        <li className={"text-center my-4"} style={{listStyle : 'none'}}   ><button className="btn btn-primary col-4 mx-1" onClick={this.props.accept}>Да</button><button className="btn btn-secondary col-4 mx-1" onClick={this.props.decline}>Отмена</button></li>
+                    </ul>}
                 </Modal.Body>
             </Modal>
         )

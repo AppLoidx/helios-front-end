210be2cd9ba9e03da41700b4428bba26f1461f92
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-31 19:15:05 +0300

    added img and mp3 loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
+const SRC = path.resolve(__dirname, 'node_modules');
+
 module.exports = {
-  entry: './jsx/App.jsx',
-  mode: "production",
+  entry: ['./jsx/App.jsx'],
+  mode: "development",
   output: {
     path:
-        '/java/helios-backend/src/main/resources/static/js'
+        '/java/helios-backend/src/main/resources/static'
         // __dirname + '/js/'
     ,
     filename: 'bundle.js'
@@ -20,7 +24,25 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loaders: ['babel-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ]},
+      {
+        test: /\.mp3$/,
+        include: SRC,
+        loader: 'file-loader'
       }
+
     ]
   }
 };

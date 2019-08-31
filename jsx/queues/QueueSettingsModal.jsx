661eda253d94f3c8274d178b3705d92202340cdb
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-31 19:11:51 +0300

    implemented shuffle function

--- jsx/queues/QueueSettingsModal.jsx
+++ jsx/queues/QueueSettingsModal.jsx
@@ -7,8 +7,31 @@ class QueueSettingsModal extends React.Component {
     constructor(props){
         super(props);
         this.state = {showDeleteConfirm: false, deleteConfirmSendingRequest: false,
-        deleteConfirmMessage : "Вы уверены, что хотите удалить очередь?"};
+        deleteConfirmMessage : "Вы уверены, что хотите удалить очередь?",
+        showShuffleModal : false, shuffleModalSendingRequest: false,
+        shuffleModalMessage : "Вы уверены, что хотите перемешать очередь?"};
         this.onDeleteAccept = this.onDeleteAccept.bind(this);
+        this.onShuffleClick = this.onShuffleClick.bind(this);
+    }
+
+    onShuffleClick(){
+        this.setState({shuffleModalSendingRequest: true});
+
+            fetch("api/queue/" + this.props.queueName +"?action=shuffle", {method: "put"})
+                .then(resp => {
+                    if (resp.status === 200){
+                        this.setState({showShuffleModal: false, shuffleModalSendingRequest: false});
+                        document.location.href = "/helios.html#/queue/" + this.props.queueName;
+                    } else {
+                        console.log("fail: " + resp.status);
+                        this.setState({showShuffleModal: false, shuffleModalSendingRequest: false});
+                    }
+                }).catch(err => {
+                console.log(err);
+                this.setState({
+                    shuffleModalSendingRequest: false,
+                    shuffleModalMessage: "Произошла ошибка. Повторите попытку позже"});
+            });
     }
 
     onDeleteAccept(){
@@ -40,21 +63,28 @@ class QueueSettingsModal extends React.Component {
                 </Modal.Header>
                 <Modal.Body>
                     <ul className="col justify-content-center">
-                       <li className={"text-center my-4"} style={{listStyle : 'none'}}   ><button className="btn btn-outline-primary col-8">Управление участниками</button></li>
-                       <li className={"text-center my-4"} style={{listStyle : 'none'}}   ><button className="btn btn-outline-primary col-8">Параметры очереди</button></li>
-                       <li className={"text-center my-4"}      style={{listStyle : 'none'}}   ><button className="btn btn-outline-danger col-8" onClick={() => this.setState({showDeleteConfirm: true})}>Удалить очередь</button></li>
+                       <li className={"text-center my-3"} style={{listStyle : 'none'}}   ><button className="btn btn-outline-primary col-8" onClick={() => this.setState({showShuffleModal: true})}>Перемешать</button></li>
+                       <li className={"text-center my-3"}      style={{listStyle : 'none'}}   ><button className="btn btn-outline-danger col-8" onClick={() => this.setState({showDeleteConfirm: true})}>Удалить очередь</button></li>
                     </ul>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.onHide} className={"btn-primary"}>Закрыть</Button>
                 </Modal.Footer>
 
-                <ConfirmModal sendingrequest={
-                            this.state.deleteConfirmSendingRequest?1:0}
-                              show={this.state.showDeleteConfirm}
-                              onHide={() => this.setState({showDeleteConfirm : false})}
-                              message={this.state.deleteConfirmMessage}
-                                accept = {() => this.onDeleteAccept()} decline={() => this.setState({showDeleteConfirm: false})}/>
+                <ConfirmModal
+                    sendingrequest={this.state.deleteConfirmSendingRequest?1:0}
+                    show={this.state.showDeleteConfirm}
+                    onHide={() => this.setState({showDeleteConfirm : false})}
+                    message={this.state.deleteConfirmMessage}
+                    accept = {() => this.onDeleteAccept()} decline={() => this.setState({showDeleteConfirm: false})}/>
+
+                <ConfirmModal
+                    sendingrequest={this.state.shuffleModalSendingRequest?1:0}
+                    show={this.state.showShuffleModal}
+                    onHide={() => this.setState({showShuffleModal: false})}
+                    message={this.state.shuffleModalMessage}
+                    accept = {this.onShuffleClick} decline={() => this.setState({showShuffleModal : false})}
+                />
             </Modal>
         )
     }

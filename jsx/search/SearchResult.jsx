31cfdee82c216bc180c0c2760949b3a95050f017
Arthur Unity <apploidyakutsk@gmail.com>
2019-08-31 19:12:51 +0300

    added on empty result message

--- jsx/search/SearchResult.jsx
+++ jsx/search/SearchResult.jsx
@@ -3,13 +3,21 @@ const RoundedSpinner = require('./../util/RoundedSpinner.jsx');
 
 
 class SearchResult extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {isEmpty : false};
+	}
+	componentWillReceiveProps(newProps){
+		this.setState({isEmpty : newProps.data.length === 0});
+	}
+
 	render(){
 		return (
 			<div className="my-3 p-3 bg-white rounded shadow-sm">
     			<h6 className="border-bottom border-gray pb-2 mb-0">Найдено:</h6>
     			<ul className={"list-group"}>
 					{this.props.loaded?
-	    				this.props.data.map((x,i) => {return <li key={i} className="list-group-item">{x}</li>})
+	    				this.state.isEmpty?"Ничего не найдено":this.props.data.map((x,i) => {return <li key={i} className="list-group-item">{x}</li>})
 	    				:
 	    				this.props.searching?<li className="mx-auto list-group-item border-0"><RoundedSpinner/></li>
 	    				:

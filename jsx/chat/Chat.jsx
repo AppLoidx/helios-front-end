7aaadfcf867131160b63019c8b33b28266496751
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-09-23 21:18:05 +0300

    lazy load for style

--- jsx/chat/Chat.jsx
+++ jsx/chat/Chat.jsx
@@ -1,6 +1,12 @@
 const React = require('react');
-const Launcher = require('react-chat-window/lib/components/Launcher.js');
-require('./../../style/chat/chat-window.css');
+let Launcher;
+let Style;
+require.ensure(['react-chat-window'], function (require) {
+	Style = require('./../../style/chat/chat-window.css');
+	Launcher = require('react-chat-window/lib/components/Launcher.js')
+}, "modal-chat");
+const Spinner = require('./../util/RoundedSpinner.jsx');
+
 
 class Chat extends React.Component {
 	constructor() {
@@ -23,11 +29,74 @@ class Chat extends React.Component {
 					}},
 			],
 			newMessagesCount: 1,
-			isOpen: false
+			isOpen: false,
+			chatModuleLoaded : false
 		};
+
+		let checkModuleLoaded = function(){
+			window.setTimeout(function (){
+				if (Launcher !== undefined && Style !==undefined){
+					this.setState({chatModuleLoaded: true})
+				}
+			}.bind(this), 1000);
+			window.setTimeout(function () {
+				if (!this.state.chatModuleLoaded) checkModuleLoaded();
+			}.bind(this), 1000);
+
+
+
+		}.bind(this);
+		window.setTimeout(checkModuleLoaded, 1000);
+		this.getMessages = this.getMessages.bind(this);
+		this.convertToMsg = this.convertToMsg.bind(this);
+	}
+
+	componentDidMount(){
+		this.getMessages();
+	}
+
+
+	getMessages(){
+		fetch(`api/chat/${this.props.queuename}?last_msg_id=0`)
+			.then(resp => resp.json())
+            .then(resp => {
+                console.log(resp);
+                resp.map( r => {
+                    this.setState({
+                        messageList: [...this.state.messageList, this.convertToMsg(r)]
+                    });
+                })
+            });
+
+	}
+
+	convertToMsg(heliosMessage){
+	    return {
+	        type : "text",
+            data : {
+	            text : heliosMessage.message
+            },
+            author : this.props.username===heliosMessage.username?"me":"them"
+        }
+
+    }
+
+	sendMessage(message){
+		// fetch(`api/chat/${this.props.queuename}?message=${message.type==="text"?message.data.text:message.data.emoji}`, {method : "put"})
+		// 	.then(resp => {
+		// 		if (resp.status === 200){
+		// 		} else {
+		// 			let msgList = this.state.messageList;
+		// 			msgList = this.state.messageList;
+		// 			msgList[msgList.length - 1] = "Не удалось отправить сообщение"
+		// 		}
+		// 	})
 	}
 
 	_onMessageWasSent(message) {
+
+		this.sendMessage(message);
+
 		this.setState({
 			messageList: [...this.state.messageList, message]
 		});
@@ -69,11 +138,10 @@ class Chat extends React.Component {
 
 	render() {
 		return <div>
-
+			{(!this.state.chatModuleLoaded)?<div style={{backgroundColor: 'transparent', position: 'fixed', right: '25px', bottom: '25px'}}><Spinner /></div>:
 			<Launcher
 				agentProfile={{
-					teamName: this.props.chatname,
-					imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+					teamName: this.props.chatname
 				}}
 				onMessageWasSent={this._onMessageWasSent.bind(this)}
 				onFilesSelected={this._onFilesSelected.bind(this)}
@@ -82,7 +150,7 @@ class Chat extends React.Component {
 				handleClick={this._handleClick.bind(this)}
 				isOpen={this.state.isOpen}
 				showEmoji
-			/>
+			/>}
 
 		</div>;
 	}

83b6e03c7a3ebe9d3c898eb1fc5dfa57643c84f2
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-13 17:33:02 +0300

    added creation date

--- jsx/queues/QueueNotification.jsx
+++ jsx/queues/QueueNotification.jsx
@@ -1,11 +1,17 @@
 const React = require('react');
 
 class QueueNotification extends React.Component {
+
     render(){
         return (
             <div className="media pt-3">
                 <p className="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong className="d-block text-gray-dark">{this.props.author}</strong>
+                    <div className="d-block text-gray-dark">
+                        <div className="d-flex justify-content-between">
+                            <div className={"d-inline"}><strong>{this.props.author}</strong></div>
+                            <div className={"d-inline"}>{this.props.time}</div>
+                        </div>
+                    </div>
                     {this.props.message}
                 </p>
             </div>

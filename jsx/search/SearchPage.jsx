6f11aa90829be164fa6767635022c1f36ffdcb1e
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-10-23 21:52:41 +0300

    changed request architect and uri

--- jsx/search/SearchPage.jsx
+++ jsx/search/SearchPage.jsx
@@ -4,74 +4,65 @@ const SearchResult = require('./SearchResult.jsx');
 const ResultQueueLink = require('./ResultQueueLink.jsx');
 
 class SearchPage extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {loaded : false, searching : false, data : [], inputVal: "", searched : false, queuesMember : []};
-		this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
-		this.onInputChange = this.onInputChange.bind(this);
-		this.isExistQueue = this.isExistQueue.bind(this);
-		$(document).bind("keypress", e => {
-			if (e.keyCode === 13) {
-				e.preventDefault();
-				this.onSearchButtonClick();
-			}
-		});
-	}
-	componentDidMount(){
-		fetch("api/user")
-			.then(resp => resp.json())
-			.then(data => {
-				let queues = data["queues_member"];
-				this.setState({queuesMember: queues});
-			});
-	}
-	onSearchButtonClick(){
-		this.setState({searching : true, loaded: false, searched : true});
-		fetch("api/check?check=queue_match&queue_name=" + this.state.inputVal)
-			.then(resp => resp.json())
-			.then(data => {
-				this.setState({data : data.map((x,i) => <ResultQueueLink shortName={x[0]} name={x[1]} joinButtonAdditionalClass={this.isExistQueue(x[0])?"btn-secondary":""}/>), searching: false, loaded: true})
-			})
-			.catch(err => {
-				console.log(err);
-				this.setState({searching : false, loaded : false});
-			});
-	};
-	isExistQueue(shortName){
-		console.log("Checking queue with name : " + shortName);
-		let queues = this.state.queuesMember;
+    constructor(props) {
+        super(props);
+        this.state = {loaded: false, searching: false, data: [], inputVal: "", searched: false, queuesMember: []};
+        this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.isExistQueue = this.isExistQueue.bind(this);
+        $(document).bind("keypress", e => {
+            if (e.keyCode === 13) {
+                e.preventDefault();
+                this.onSearchButtonClick();
+            }
+        });
+    }
 
-		if (queues !== undefined) {
-			console.log("1");
-			for (let que of queues) {
-				if (que[0] === shortName){
-					console.log("return true");
-					return true;
-				}
-			}
-		}
-		console.log("return false");
-		return false;
+    onSearchButtonClick() {
+        this.setState({searching: true, loaded: false, searched: true});
+        fetch("search/queue?queue_name=" + this.state.inputVal)
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState({
+                    data: data.map((x, i) => <ResultQueueLink
+                            shortName={x["queue_name"]}
+                            name={x["queue_fullname"]}
+                            alreadyExistInQueue={x["already_in_queue"]}
+                            isPrivate={x["private"]}
+                            membersCount={x["members_count"]}
+                            creationDate={x["creation_date"]}
+                        />
+                    ), searching: false, loaded: true
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({searching: false, loaded: false});
+            });
+    };
 
-	}
+    onInputChange(value) {
+        this.setState({inputVal: value});
+    };
 
-	onInputChange(value){
-		this.setState({inputVal : value});
-	};
-
-	render(){
-		return (
-			<div className="container">
-				<br/>
-				<div className="row justify-content-center">
-					<div className="col-12 col-md-12 col-lg-10">
-					   <div data-aos="slide-up" data-aos-duration="800"><SearchBar onSearchButtonClick={this.onSearchButtonClick} onInputChange={this.onInputChange}/></div>
-					   <div data-aos="slide-up" data-aos-duration="1100"><SearchResult loaded={this.state.loaded} searching={this.state.searching} data={this.state.data} searched={this.state.searched}/></div>
-					</div>
-				</div>
-			</div>
-			)
-	}
+    render() {
+        return (
+            <div className="container">
+                <br/>
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-12 col-lg-10">
+                        <div data-aos="slide-up" data-aos-duration="800"><SearchBar
+                            onSearchButtonClick={this.onSearchButtonClick} onInputChange={this.onInputChange}/></div>
+                        <div data-aos="slide-up" data-aos-duration="1100"><SearchResult loaded={this.state.loaded}
+                                                                                        searching={this.state.searching}
+                                                                                        data={this.state.data}
+                                                                                        searched={this.state.searched}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 module.exports = SearchPage;

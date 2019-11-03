7c06dafccc5a8dd4a4585732d94e0e6d85114d27
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2019-11-04 00:31:17 +0300

    added date parse

--- jsx/search/ResultQueueLink.jsx
+++ jsx/search/ResultQueueLink.jsx
@@ -3,14 +3,27 @@ const Spinner = require('./../util/RoundedSpinner.jsx');
 const ModalWindow = require('./../util/QueuePasswordModal.jsx');
 
 class ResultQueueLink extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {sendingReq: false, showModal: false};
+        this.state = {sendingReq: false, showModal: false, creationDate: null};
         this.onJoinButtonClick = this.onJoinButtonClick.bind(this);
+
     }
 
+    componentDidMount() {
+        let a = this.props.creationDate.split(/[^0-9]/);
+        let creationDate = new Date(Date.UTC(a[0], a[1] - 1, a[2], a[3], a[4], a[5]));
+        const DATE = creationDate.getDate();
+        const MONTH = creationDate.toLocaleString('default', {month: 'short'});
+        const HOURS = creationDate.getHours();
+        const MINUTES = creationDate.getMinutes();
+        this.setState({creationDate: `${HOURS}:${MINUTES} ${DATE} ${MONTH}`});
+    }
+
+
     onJoinButtonClick() {
-        if (this.state.joinButtonAdditionalClass === "btn-secondary") return;
+        if (this.props.alreadyExistInQueue) return;
         this.setState({sendingReq: true});
         if (this.props.isPrivate) this.setState({showModal: true});
         else
@@ -45,9 +58,8 @@ class ResultQueueLink extends React.Component {
                         </div>
                     </div>
                     <span className="d-block">/{this.props.shortName}</span>
-                    <span
-                        className="d-inline-block text-secondary">Количество участников: {this.props.membersCount}</span>
-                    <span className="d-block text-secondary">Дата создания: {this.props.creationDate}</span>
+                    <span className="d-block">Количество участников: {this.props.membersCount}</span>
+                    <span className="d-block text-secondary">Дата создания: {this.state.creationDate}</span>
                 </div>
             </div>
         )
